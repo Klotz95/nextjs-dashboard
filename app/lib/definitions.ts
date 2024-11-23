d7841392e8d6a18b1e43403e85946f0302c77d78
NKO <NicoKotlenga@live.de>
2024-11-23 10:11:53 +0100

    End chapter 11
    * Project state after finishing chapter 11 of nextjs tutorial

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -16,6 +16,8 @@ export type Customer = {
   image_url: string;
 };
 
+export type InvoiceStatus = "pending" | "paid"
+
 export type Invoice = {
   id: string;
   customer_id: string;
@@ -23,8 +25,13 @@ export type Invoice = {
   date: string;
   // In TypeScript, this is called a string union type.
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
-  status: 'pending' | 'paid';
+  status: InvoiceStatus;
 };
+
+export type InvoiceCountByStatus = {
+  status: InvoiceStatus;
+  count: number
+}
 
 export type Revenue = {
   month: string;
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,58 +1,61 @@
 import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
+    BanknotesIcon,
+    ClockIcon,
+    UserGroupIcon,
+    InboxIcon,
 } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import {lusitana} from '@/app/ui/fonts';
+import {fetchCustomerCount, fetchInvoicesCountByStatus} from "@/app/lib/data";
 
 const iconMap = {
-  collected: BanknotesIcon,
-  customers: UserGroupIcon,
-  pending: ClockIcon,
-  invoices: InboxIcon,
+    collected: BanknotesIcon,
+    customers: UserGroupIcon,
+    pending: ClockIcon,
+    invoices: InboxIcon,
 };
 
 export default async function CardWrapper() {
-  return (
-    <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
+    const [invoiceCountByStatus, customerCount] = await Promise.all([fetchInvoicesCountByStatus(), fetchCustomerCount()])
+    const numberOfInvoices = Array.from(invoiceCountByStatus.values()).reduce((accumulator, current) => accumulator + current, 0);
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
-    </>
-  );
+
+    return (
+        <>
+            <Card title="Collected" value={invoiceCountByStatus.get("paid") ?? 0} type="collected"/>
+            <Card title="Pending" value={invoiceCountByStatus.get("pending") ?? 0} type="pending"/>
+            <Card title="Total Invoices" value={numberOfInvoices} type="invoices"/>
+            <Card
+                title="Total Customers"
+                value={customerCount}
+                type="customers"
+            />
+        </>
+    );
 }
 
 export function Card({
-  title,
-  value,
-  type,
-}: {
-  title: string;
-  value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+                         title,
+                         value,
+                         type,
+                     }: {
+    title: string;
+    value: number | string;
+    type: 'invoices' | 'customers' | 'pending' | 'collected';
 }) {
-  const Icon = iconMap[type];
+    const Icon = iconMap[type];
 
-  return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-      <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
-      </div>
-      <p
-        className={`${lusitana.className}
+    return (
+        <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
+            <div className="flex p-4">
+                {Icon ? <Icon className="h-5 w-5 text-gray-700"/> : null}
+                <h3 className="ml-2 text-sm font-medium">{title}</h3>
+            </div>
+            <p
+                className={`${lusitana.className}
           truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
-      >
-        {value}
-      </p>
-    </div>
-  );
+            >
+                {value}
+            </p>
+        </div>
+    );
 }
